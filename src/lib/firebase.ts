f0f2b77aeece5948583f785860126f71ce06e36b
Is320-source <isidromanuel39@gmail.com>
2026-05-27 14:54:33 +0100

    feat: new feacture

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase only if it hasn't been initialized yet
+// Initialize Firebase only if it hasn't been initialized yet 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
